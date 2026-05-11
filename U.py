1ef47519f2e1cd17d4362b57d74b2cf42d20904e
pankajbishnoi18 <pankajbishnoi2916@gmail.com>
2026-05-11 08:29:01 +0530

    did some improvisation in code and added the note delete feature

--- U.py
+++ U.py
@@ -1,26 +1,48 @@
 note1="hey there this is the first note"
 note2="this is the second one"
-existing_notes=[f"1-{note1}  ,  2-{note2}"]  
-print("select the command you bkhkhggkhkhhbg want to execute")
-print("1-add a new note")
-print("2-view the existing notes")
-i=int(input())
+existing_notes=[note1,note2]  
 
-if i==1:
-    print("type name of the note")
-    name=input()
-    print("now type whatver you want to write")
-    text=input()
-    note3=text
-    print("do you want to save the note to the existing notes ,answer in yes or no only")
-    resp=input()
-    if resp=="yes":
-        existing_notes.append(f"3-{note3}")
-        print("saved successfully")
-        print(existing_notes)
-if i==2:
-    for i in range(len(existing_notes)):
-        print(f"{i+1}--{existing_notes[i]}")
+j=1
+while j>0:
+
+    print("select the command you  want to execute")
+    print("1-add a new note")
+    print("2-view the existing notes")
+    print("20-to delete any note")
+    print("press 16 to exit the app")
+    i=int(input())
+    if i==1:
+    
+        print("now type whatver you want to write")
+        text=input()
+        note3=text
+        print("do you want to save the note to the existing notes ,answer in yes or no only")
+        resp=input()
+        if resp=="yes":
+           existing_notes.append(note3)
+           print("saved successfully")
+        if resp=="no":
+           continue
+           
+    if i==2:
+     for k in range(len(existing_notes)):
+        print(f"{k+1}--{existing_notes[k]}")
+    print("_______________________________________________________________________________________________________")
+    print("_______________________________________________________________________________________________________")
+    if i==16:
+         break
+    if i==20:
+        for k in range(len(existing_notes)):
+           print(f"{k+1}--{existing_notes[k]}")
+        print("enter the number of the note which you want to delete")
+        d=int(input())
+        existing_notes.pop(d-1)
+        print(f"note no {d} is deleted successfully")
+
+
+
+
+
 
 
 
